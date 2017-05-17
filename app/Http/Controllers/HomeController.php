e1a8cf2923cc8bc3e2285278652cfa4ebaa72ec9
aethr <lindsay.gaines@gmail.com>
2017-05-17 13:48:27 +1000

    Redirect to user register pages on fresh install

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\User;
 use Barryvdh\Debugbar\Facade as Debugbar;
 
 class HomeController extends Controller
@@ -16,7 +17,21 @@ class HomeController extends Controller
     {
         $items = Item::all();
 
-        Debugbar::info($items);
+        // No items in the database, possibly a new installation?
+        if (!count($items)) {
+            $users = User::all();
+
+            // If no items, and no users are present, we can assume a fresh
+            // install, redirect the user to the registration page
+            if (!count($users)) {
+                return redirect()
+                    ->route('register');
+
+                // TODO add message support to base template
+                //    ->with('message', 'Welcome to neocortex! Please start by creating a user.');
+            }
+        }
+
         return view('display', ['items' => $items]);
     }
 }
