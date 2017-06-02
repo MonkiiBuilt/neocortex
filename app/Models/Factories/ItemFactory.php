693bf1bf4c7bf5b3db1e8d99af338d9d5c7b8307
aethr <lindsay.gaines@gmail.com>
2017-06-02 11:26:09 +1000

    Remove superfluous methods from ItemFactory

--- app/Models/Factories/ItemFactory.php
+++ app/Models/Factories/ItemFactory.php
@@ -36,30 +36,6 @@ class ItemFactory
     }
 
 
-    /**
-     * Given a URL, provide a weighted value indicating whether the URL is
-     * likely to served by this Item type.
-     *
-     * Note: this method should only parse the URL for patterns, ItemType
-     * controllers should avoid making a request to the URL. If no match is
-     * found based on the URL, the system will make a HEAD request to te URL
-     * and pass it to the matchHeaders method.
-     *
-     * @param $url
-     * @return integer The 'weight' of a possible match, with 0 meaning "no match".
-     */
-    public static function matchByURL($url) { return 0; }
-
-
-    /**
-     * If no URL match is found, a HEAD request will be made to the URL so that
-     * type matching can be performed on HTTP headers, for example mime type.
-     *
-     * @param $headers
-     * @return integer The 'weight' of a possible match, with 0 meaning "no match".
-     */
-    public static function matchByHeaders($headers) { return 0; }
-
 
     /**
      * Attempt to identify the item type based on the available attributes.
